33270e208eebbec502c0a78e1bce650c0d37cf1f
oguzhan <ozgokoguzhan34@gmail.com>
2023-12-01 12:34:04 +0300

    added sum function

--- src/main/kotlin/FirstProgram.kt
+++ src/main/kotlin/FirstProgram.kt
@@ -3,4 +3,9 @@ class FirstProgram {
     fun main() {
         println("Hello World!")
     }
+    fun sum() {
+        val a = 1
+        val b = 2
+        println("sum of $a and $b is ${a + b}")
+    }
 }
