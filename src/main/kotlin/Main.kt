62154a3a4f7e2dbb0632bf6320f5bd9667a16638
oguzhan <ozgokoguzhan34@gmail.com>
2023-12-01 13:15:30 +0300

    Created an object and called the function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,6 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
-    val firstProgram = FirstProgram()
-    firstProgram.sum()
+    val sumClass = SumClass()
+    sumClass.sum()
 }
--- src/main/kotlin/SumClass.kt
+++ src/main/kotlin/SumClass.kt
@@ -1,4 +1,4 @@
-class FirstProgram {
+class SumClass {
     fun sum() {
         val a = 1
         val b = 2
