8353735dbab366d36144966acbc8e22d971504d0
oguzhan <ozgokoguzhan34@gmail.com>
2023-12-01 13:44:19 +0300

    exponentiation class created and called

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,17 @@ import gettingStarted.*
 import gettingStarted.SumClass
 import kotlin.math.pow
 
+/**
+ * This is a documentation comment
+ */
+
+// This is a single line comment
+
+/*
+This is a multi line comment
+Created by: Oguzhan OZGOKCE
+ */
+
 fun main(args: Array<String>) {
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
