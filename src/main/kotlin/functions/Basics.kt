8e8531371a5f7487b9a110a15d69f547a26a9f3a
oguzhan <ozgokoguzhan34@gmail.com>
2023-12-03 13:46:23 +0300

    I created functions blocks

--- src/main/kotlin/functions/Basics.kt
+++ src/main/kotlin/functions/Basics.kt
@@ -1,5 +1,27 @@
 package functions
 
 fun main() {
+    /**
+     *  fun keyword -> function -> function name -> parameters() -> return type -> function body -> return value
+     *  Unit  -> not parameter -> not return value -> void
+     *  while calling the function -> function name -> parameters
+     *  When calling a class's function -> object name -> function name -> parameters
+     */
+
+    helloWorld()
+
+    val square = takeSquare(5)
+    println("$square")
+    //or
+    println("Number Square : ${takeSquare(5)}")
+    //or
+    println("Number Square : ${takeSquare(number = 5)}")
 
 }
+fun helloWorld() : Unit {
+    println("Hello World")
+}
+fun takeSquare(number : Int ) : Int {
+    return number * number
+}
+
