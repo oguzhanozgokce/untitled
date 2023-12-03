627449af26804ad639c03917a482d210a8ff3444
oguzhan <ozgokoguzhan34@gmail.com>
2023-12-03 14:19:55 +0300

    I created basics functions blocks

--- src/main/kotlin/functions/Basics.kt
+++ src/main/kotlin/functions/Basics.kt
@@ -1,5 +1,7 @@
 package functions
 
+import kotlin.math.pow
+
 fun main() {
     /**
      *  fun keyword -> function -> function name -> parameters() -> return type -> function body -> return value
@@ -17,6 +19,11 @@ fun main() {
     //or
     println("Number Square : ${takeSquare(number = 5)}")
 
+    println(2.0.pow(3.0)) // 2^3 = 8.0
+    reformatMessage("Engineering", true, 10, "EN")
+    reformatMessage("ComputerEngineering", size = 10, lang = "TR")
+    reformatMessage("AndroidDeveloper", size = 10)
+
 }
 fun helloWorld() : Unit {
     println("Hello World")
@@ -24,4 +31,15 @@ fun helloWorld() : Unit {
 fun takeSquare(number : Int ) : Int {
     return number * number
 }
+
+//----------------------------------------------------------------------------------------------------
+
+/**
+ * Default Parameters - > Default value assignment allows us to do function overload -> Turkish translate: Default deger ataması yapmak  fonksiyon overload islemi yapmamizi saglar
+ */
+fun reformatMessage(message  : String, isUpperCase: Boolean = false , size: Int , lang: String = "TR") {
+    println("message : $message , isUpperCase : $isUpperCase , size : $size , lang : $lang")
+
+}
+
 
