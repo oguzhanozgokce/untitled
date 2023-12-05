4eedff3f2dc89de2aa2f3f8fbb10a4630acfe4ba
oguzhan <ozgokoguzhan34@gmail.com>
2023-12-06 01:18:52 +0300

    I created Extension functions blocks

--- src/main/kotlin/functions/ExtensionFunctions.kt
+++ src/main/kotlin/functions/ExtensionFunctions.kt
@@ -1,5 +1,7 @@
 package functions
 
+
+
 // Code with ♥️
 // _______________________________
 // |					         |
@@ -10,7 +12,32 @@ package functions
 fun main() {
     /**
      *Extension functions are used to add new functions to existing classes.
+     * Receiver class needs to be defined first. Then the function is defined. Then the function is called.
+     * fun receiverClass.functionName(){
+     *     // function body
      *
      */
+
+    (3+0.14).log("Number : ")
+
+    log2(3+0.14)
+
+    3 + 0.14 log "Number : "  // infix function
+
+    val result : Int = "3" extPlus "5"
+    println(result)
+
+
+
+}
+
+
+fun log2(number : Number){ // normal function
+    println(number)
+}
+infix fun String.extPlus(otherString: String ) : Int = this.toInt() + otherString.toInt()
+
+infix  fun Number.log(empty : String){  // infix function and extension function
+    println(empty + this)
 }
 
